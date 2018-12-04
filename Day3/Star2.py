a8ee408cd038bb894a48ac5a31861ac873f2a0fc
Hannah Guo <hguo2102@gmail.com>
2018-12-03 22:45:59 -0500

    OPTIMIZED, things now run at a decent speed!!

--- Day3/Star2.py
+++ Day3/Star2.py
@@ -1,20 +1,12 @@
-import time
-millis = int(round(time.time() * 1000))
-
 inputText = [line.rstrip('\n') for line in open('input.txt')]
 
-leftTop = []
-widthHeight = []
 rects = []
 blanket = [["X" for j in range(1000)] for i in range(1000)]
+numList = list(range(1, len(inputText) + 1))
 
 for i in range(len(inputText)):
-    leftTop.append(inputText[i][inputText[i].index("@"):].split(":", 1)[0].strip()[2:])
-    leftTop[i] = leftTop[i].split(",")
-    widthHeight.append((inputText[i][inputText[i].index(":"):][2:]))
-    widthHeight[i] = widthHeight[i].split("x")
-
-    rects.append([int(leftTop[i][0]), int(leftTop[i][1]), int(widthHeight[i][0]), int(widthHeight[i][1])])
+    rects.append([int(x) for x in (inputText[i][inputText[i].index("@"):].split(":", 1)[0].strip()[2:].split(",")) +
+                  (inputText[i][inputText[i].index(":"):][2:]).split("x")])
 
     for j in range(rects[i][2]):
         for k in range(rects[i][3]):
@@ -22,13 +14,14 @@ for i in range(len(inputText)):
             if blank == "X" and blank != 0:
                 blanket[j + rects[i][0]][k + rects[i][1]] = i + 1
             else:
+                if numList.__contains__(i + 1):
+                    numList.remove(i + 1)
+                elif numList.__contains__(blank):
+                    numList.remove(blank)
                 blanket[j + rects[i][0]][k + rects[i][1]] = 0
 
-oneD = sum(blanket, [])
 
-for m in range(1, len(inputText) + 1):
-    if oneD.count(m) == (rects[m - 1][2] * rects[m - 1][3]):
+for m in numList:
+    if sum(x.count(m) for x in blanket) == (rects[m - 1][2] * rects[m - 1][3]):
         print("FINAL: " + str(m))
         break
-
-print(int(round(time.time() * 1000)) - millis)
